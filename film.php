f5df7f8ab091cdc3f6082d1f69158c4a0556aeb7
Umberto Loria SAM <umberto.loria@gmail.com>
2019-12-22 16:49:03 +0100

    Pulizia invio dati per pagina film

--- film.php
+++ film.php
@@ -1,51 +1,43 @@
 <?php
 include "parts/initial_page.php";
-$id = @$_GET["id"];
-if ($id === null) {
-	echo "non mi hai dato un id";
-} elseif (!ctype_digit($id)) {
-	echo "dammi un numero per id";
-} elseif (!$film = FilmManager::doRetrieveByID($id)) {
-	echo "film non trovato";
-} else {
-	unset($id);
-	$artisti = [];
-	$recitazioni = RecitazioneManager::doRetrieveByFilm($film->getID());
-	foreach ($recitazioni as $recitazione) {
-		if (!array_key_exists($recitazione->getAttore(), $artisti)) {
-			$artisti[$recitazione->getAttore()] = ArtistaManager::doRetrieveByID($recitazione->getAttore());
-		}
-	}
-	$registi = RegiaManager::get_artisti_from_film($film->getID());
-	foreach ($registi as $regista) {
-		if (!array_key_exists($regista, $artisti)) {
-			$artisti[$regista] = ArtistaManager::doRetrieveByID($regista);
-		}
-	}
-	$_REQUEST["film"] = $film;
-	$_REQUEST["recitazioni"] = $recitazioni;
-	$_REQUEST["registi"] = $registi;
-	$_REQUEST["artisti"] = $artisti;
-	$_REQUEST["generi"] = GenereManager::doRetrieveByFilm($film->getID());
+$film = FilmManager::doRetrieveByID(@$_GET["id"]);
+if (!$film)
+	die("film non trovato");
 
-	$_REQUEST["show_actions"] = [];
-	$logged_user = Auth::getLoggedUser();
-	if ($logged_user) {
-		// FIXME: Spreco di memoria!
-		if (count(AmiciziaManager::getFriendships($logged_user->getID())) > 0)
-			$_REQUEST["show_actions"][] = "suggest";
-		if (!FilmGuardatiManager::exists($logged_user->getID(), $film->getID()))
-			$_REQUEST["show_actions"][] = "add_film_guardato";
-		if (!FilmDaGuardareManager::exists($logged_user->getID(), $film->getID()))
-			$_REQUEST["show_actions"][] = "add_film_da_guardare";
-		if (!empty(ListaManager::getAllOf($logged_user->getID())))
-			$_REQUEST["show_actions"][] = "add_to_liste";
-	}
-	unset($logged_user);
+$recitazioni = RecitazioneManager::doRetrieveByFilm($film->getID());
+$registi = RegiaManager::get_artisti_from_film($film->getID());
+$_REQUEST["recitazioni"] = $recitazioni;
+$_REQUEST["registi"] = $registi;
 
-	unset($film);
-	unset($recitazioni);
-	unset($registi);
-	unset($artisti);
-	include "views/Pagina film.php";
+$artisti = [];
+foreach ($recitazioni as $recitazione)
+	$artisti[$recitazione->getAttore()] = null;
+foreach ($registi as $regista)
+	$artisti[$regista] = null;
+foreach ($artisti as $artista_id => $_)
+	$artisti[$artista_id] = ArtistaManager::doRetrieveByID($artista_id);
+$_REQUEST["artisti"] = $artisti;
+unset($recitazioni);
+unset($registi);
+unset($artisti);
+
+$_REQUEST["generi"] = GenereManager::doRetrieveByFilm($film->getID());
+
+$_REQUEST["show_actions"] = [];
+$logged_user = Auth::getLoggedUser();
+if ($logged_user) {
+	// FIXME: Spreco di memoria!
+	if (count(AmiciziaManager::getFriendships($logged_user->getID())) > 0)
+		$_REQUEST["show_actions"][] = "suggest";
+	if (!FilmGuardatiManager::exists($logged_user->getID(), $film->getID()))
+		$_REQUEST["show_actions"][] = "add_film_guardato";
+	if (!FilmDaGuardareManager::exists($logged_user->getID(), $film->getID()))
+		$_REQUEST["show_actions"][] = "add_film_da_guardare";
+	if (!empty(ListaManager::getAllOf($logged_user->getID())))
+		$_REQUEST["show_actions"][] = "add_to_liste";
 }
+unset($logged_user);
+
+$_REQUEST["film"] = $film;
+unset($film);
+include "views/Pagina film.php";
