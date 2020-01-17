b2c13e5e65fd3a8a96bda3a2b76003d9c41970f7
Umberto Loria SAM <umberto.loria@gmail.com>
2020-01-17 16:00:07 +0100

    Miglioramento test di AccountDAO

--- tests/GenericTest.php
+++ tests/GenericTest.php
@@ -4,6 +4,7 @@
  * @method assertNull($obj)
  * @method assertEquals($a, $b)
  * @method assertTrue($test)
+ * @method assertFalse($test)
  * @method assertNotNull($obj)
  */
 class GenericTest extends PHPUnit\Framework\TestCase {
--- tests/account/AccountDAOTest.php
+++ tests/account/AccountDAOTest.php
@@ -8,132 +8,142 @@ include_once "../stubs/StubAccountDAO.php";
 class AccountDAOTest extends GenericTest {
 
 	private static $account_dao;
-	/** @type Utente[] */
-	private static $saves = [];
 
 	public static function setUpBeforeClass(): void {
-//		AccountDAOFactory::useStub();
+		AccountDAOFactory::useStub(); // per testare il DB bisogna avere gli id 1, 2, 3 liberi ed auto_increment = 1
 		self::$account_dao = AccountDAOFactory::getAccountDAO();
 	}
 
-	private function realExistsTest($nome, $cognome, $email, $password, $isGestore) {
-		$utente = new Utente(0, $nome, $cognome, $email, sha1($password), $isGestore);
+	private function assertMatches(Utente $result, $oracle) {
+		$this->assertNotNull($result);
+		$this->assertEquals($result->getNome(), $oracle[0]);
+		$this->assertEquals($result->getCognome(), $oracle[1]);
+		$this->assertEquals($result->getEmail(), $oracle[2]);
+		$this->assertEquals($result->getPassword(), sha1($oracle[3]));
+		$this->assertEquals($result->isGestore(), $oracle[4]);
+	}
 
-		$oracle = false;
-		foreach (self::$saves as $save)
-			if ($save->getEmail())
-				$oracle = true;
+	public function existsPre() {
+		return [
+			'mario rossi' => ["mario@rossi.lol", false],
+			'michelantonio' => ["mama@non.mama", false],
+		];
+	}
 
-		$result = self::$account_dao->exists($utente->getEmail());
+	/** @dataProvider existsPre */
+	public function testExistsBeforeCreation($email, $oracle) {
+		$this->assertEquals(
+			self::$account_dao->exists($email),
+			$oracle
+		);
+	}
 
+	public function create() {
+		return [
+			'mario rossi' => ["Mario", "Rossi", "mario@rossi.lol", "provaprova", true,
+				["Mario", "Rossi", "mario@rossi.lol", "provaprova", true]],
+			'michelantonio' => ["Michelantonio", "Panichella", "mama@non.mama", "molise", false,
+				["Michelantonio", "Panichella", "mama@non.mama", "molise", false]],
+			'michelantonio (già esiste)' => ["Michelantonio", "Panichella", "mama@non.mama", "molise", true, []],
+		];
+	}
+
+	/** @dataProvider create */
+	public function testCreateUser($nome, $cognome, $email, $password, $isGestore, $oracle) {
+		$result = self::$account_dao->create(
+			new Utente(0, $nome, $cognome, $email, sha1($password), $isGestore)
+		);
+		if (empty($oracle))
+			$this->assertNull($result);
+		else
+			$this->assertMatches($result, $oracle);
+	}
+
+	public function existsPost() {
+		return [
+			'mario rossi' => ["mario@rossi.lol", true],
+			'michelantonio' => ["mama@non.mama", true],
+		];
+	}
+
+	/** @dataProvider existsPost */
+	public function testExistsAfterCreation($email, $oracle) {
+		$this->assertEquals(
+			self::$account_dao->exists($email),
+			$oracle
+		);
+	}
+
+	public function get_from() {
+		return [
+			'mario rossi' => [1, ["Mario", "Rossi", "mario@rossi.lol", "provaprova", true]],
+			'michelantonio' => [2, ["Michelantonio", "Panichella", "mama@non.mama", "molise", false]],
+			'michelantonio (già esiste)' => [3, []],
+		];
+	}
+
+	/** @dataProvider get_from */
+	public function testGetFromID($id, $oracle) {
+		$result = self::$account_dao->get_from_id($id);
+		if (empty($oracle))
+			$this->assertNull($result);
+		else
+			$this->assertMatches($result, $oracle);
+	}
+
+	public function update() {
+		return [
+			'mario rossi' => [1, "ciaociao", ["Mario", "Rossi", "mario@rossi.lol", "ciaociao", true]],
+			'michelantonio' => [2, "molisana", ["Michelantonio", "Panichella", "mama@non.mama", "molisana", false]],
+			'non esiste' => [3, "molisana", []],
+		];
+	}
+
+	/** @dataProvider update */
+	public function testUpdateUser($id, $nuova_password, $oracle) {
+		$utente = self::$account_dao->get_from_id($id);
+		if (empty($oracle)) {
+			$this->assertNull($utente);
+		} else {
+			$this->assertNotNull($utente);
+			$utente->setPassword(sha1($nuova_password));
+			$result = self::$account_dao->update($utente);
+			$this->assertMatches($result, $oracle);
+		}
+	}
+
+	public function authenticate() {
+		return [
+			'mario rossi' => ["mario@rossi.lol", "ciaociao", ["Mario", "Rossi", "mario@rossi.lol", "ciaociao", true]],
+			'michelantonio' => ["mama@non.mama", "molisana", ["Michelantonio", "Panichella", "mama@non.mama", "molisana", false]],
+			'michelantonio (vecchia password)' => ["mama@non.mama", "molise", []],
+		];
+	}
+
+	/** @dataProvider authenticate */
+	public function testAuthenticateUser($email, $password, $oracle) {
+		$result = self::$account_dao->authenticate($email, sha1($password));
+		if (empty($oracle))
+			$this->assertNull($result);
+		else
+			$this->assertMatches($result, $oracle);
+	}
+
+	public function delete() {
+		return [
+			'mario rossi' => [1, true],
+			'michelantonio' => [2, true],
+			'non esiste' => [3, false],
+		];
+	}
+
+	/** @dataProvider delete */
+	public function testDeleteUser($id, $oracle) {
+		$result = self::$account_dao->delete($id);
 		$this->assertEquals($result, $oracle);
-	}
-
-	private function utenti_uguali(Utente $utente1, Utente $utente2): bool {
-		return $utente1->getNome() == $utente2->getNome() and
-			$utente1->getCognome() == $utente2->getCognome() and
-			$utente1->getEmail() == $utente2->getEmail() and
-			$utente1->getPassword() == $utente2->getPassword() and
-			$utente1->isGestore() == $utente2->isGestore();
-	}
-
-	public function create_and_retrieve_users_provider() {
-		return [
-			'mario rossi' => ["Mario", "Rossi", "mario@rossi.lol", "provaprova", false],
-			'michelantonio' => ["Michelantonio", "Panichella", "mama@non.mama", "molise", true],
-		];
-	}
-
-	/** @dataProvider create_and_retrieve_users_provider */
-	public function testExistsBeforeCreation($nome, $cognome, $email, $password, $isGestore) {
-		$this->realExistsTest($nome, $cognome, $email, sha1($password), $isGestore);
-	}
-
-	/** @dataProvider create_and_retrieve_users_provider */
-	public function testCreateUser($nome, $cognome, $email, $password, $isGestore) {
-		$utente = new Utente(0, $nome, $cognome, $email, sha1($password), $isGestore);
-		$utente_salvato = self::$account_dao->create($utente);
-		$this->assertNotNull($utente_salvato);
-		$this->assertTrue($this->utenti_uguali($utente_salvato, $utente));
-		self::$saves[] = $utente_salvato;
-	}
-
-	/** @dataProvider create_and_retrieve_users_provider */
-	public function testExistsAfterCreation($nome, $cognome, $email, $password, $isGestore) {
-		$this->realExistsTest($nome, $cognome, $email, sha1($password), $isGestore);
-	}
-
-	/** @dataProvider create_and_retrieve_users_provider */
-	public function testGetFromID($nome, $cognome, $email, $password, $isGestore) {
-		$app = new Utente(0, $nome, $cognome, $email, sha1($password), $isGestore);
-		$utente = null;
-		foreach (self::$saves as $save)
-			if ($this->utenti_uguali($app, $save))
-				$utente = $save;
-		assert(!is_null($utente));
-		$utente_salvato = self::$account_dao->get_from_id($utente->getID());
-		$this->assertNotNull($utente_salvato);
-		$this->assertTrue($this->utenti_uguali($utente_salvato, $utente));
-	}
-
-	public function update_users_provider() {
-		return [
-			'mario rossi' => ["Mario", "Rossi", "mario@rossi.lol", "provaprova", false, "ciaociao"],
-			'michelantonio' => ["Michelantonio", "Panichella", "mama@non.mama", "molise", true, "molisana"],
-		];
-	}
-
-	/** @dataProvider update_users_provider */
-	public function testUpdateUser($nome, $cognome, $email, $password, $isGestore, $nuova_password) {
-		$app = new Utente(0, $nome, $cognome, $email, sha1($password), $isGestore);
-		$utente = null;
-		foreach (self::$saves as $save)
-			if ($this->utenti_uguali($app, $save))
-				$utente = $save;
-		assert(!is_null($utente));
-		$utente->setPassword(sha1($nuova_password));
-		$utente_salvato = self::$account_dao->update($utente);
-		$this->assertTrue($this->utenti_uguali($utente, $utente_salvato));
-	}
-
-	public function authenticate_users_provider() {
-		return [
-			'mario rossi' => ["mario@rossi.lol", "ciaociao"],
-			'michelantonio' => ["mama@non.mama", "molisana"],
-		];
-	}
-
-	/** @dataProvider authenticate_users_provider */
-	public function testAuthenticateUser($email, $password) {
-		$oracle = null;
-		foreach (self::$saves as $save)
-			if ($save->getEmail() == $email and $save->getPassword() == sha1($password))
-				$oracle = $save;
-		assert(!is_null($oracle));
-		$utente_loggato = self::$account_dao->authenticate($email, sha1($password));
-		$this->assertNotNull($utente_loggato);
-		$this->assertTrue($this->utenti_uguali($utente_loggato, $oracle));
-	}
-
-	public function delete_users_provider() {
-		return [
-			'mario rossi' => ["Mario", "Rossi", "mario@rossi.lol", "ciaociao", false],
-			'michelantonio' => ["Michelantonio", "Panichella", "mama@non.mama", "molisana", true],
-		];
-	}
-
-	/** @dataProvider delete_users_provider */
-	public function testDeleteUser($nome, $cognome, $email, $password, $isGestore) {
-		$app = new Utente(0, $nome, $cognome, $email, sha1($password), $isGestore);
-		$save_key = -1;
-		foreach (self::$saves as $key => $save)
-			if ($this->utenti_uguali($app, $save))
-				$save_key = $key;
-		$utente = self::$saves[$save_key];
-		assert(!is_null($utente));
-		unset(self::$saves[$save_key]);
-		$res = self::$account_dao->delete($utente->getID());
-		$this->assertTrue($res);
+		$this->assertNull(
+			self::$account_dao->get_from_id($id)
+		);
 	}
 
 	public function testRicerca() {
