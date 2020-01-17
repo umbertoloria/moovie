8b6a5d3af4479bbcdf88211f5565dce5ba1bc139
Umberto Loria SAM <umberto.loria@gmail.com>
2020-01-18 00:50:37 +0100

    Aggiustamento test di Modifica genere

--- tests/genere/ModificareGenereTest.php
+++ tests/genere/ModificareGenereTest.php
@@ -7,90 +7,83 @@ include_once "../stubs/StubGenereDAO.php";
 
 class ModificareGenereTest extends GenericTest {
 
-    // --do-not-cache-result
+	private static $account_dao;
+	private static $userid_normale;
+	private static $userid_gestore;
+	private static $genere_dao;
+	private static $drammatico;
+	private static $inesistente;
 
-    private static $account_dao;
-    private static $account2_dao;
-    private static $genere_dao;
-    private static $userid;
-    private static $user2id;
-    private static $genereid;
+	public static function setUpBeforeClass(): void {
+		Testing::init();
+//		AccountDAOFactory::useStub();
+		self::$account_dao = AccountDAOFactory::getAccountDAO();
+		self::$userid_normale = self::$account_dao->create(
+			new Utente(0, "Giuseppe", "Verdi", "g.verdi@gmail.com", sha1("140898"))
+		)->getID();
+		self::$userid_gestore = self::$account_dao->create(
+			new Utente(0, "Giuseppe", "Antoniani", "g.antoniani@gmail.com", sha1("140898"), true)
+		)->getID();
+//		GenereDAOFactory::useStub();
+		self::$genere_dao = GenereDAOFactory::getGenereDAO();
+		self::$drammatico = self::$genere_dao->create(new Genere(0, "Drammatico"));
+		self::$inesistente = self::$genere_dao->create(new Genere(0, "Inesistente"));
+	}
 
-    public static function setUpBeforeClass(): void {
-        Testing::init();
-//      AccountDAOFactory::useStub();
-//      GenereDAOFactory::useStub(); // affiché il test dei DAO possa funzionare, bisogna creare un nuovo genere (che sarà memorizzato negli stub) chiamato "comico", decommentare le righe 32 e 33
-        self::$account_dao = AccountDAOFactory::getAccountDAO();
-        self::$account2_dao = AccountDAOFactory::getAccountDAO();
-        self::$genere_dao = GenereDAOFactory::getGenereDAO();
-        $real_utente = self::$account_dao->create(
-            new Utente(0, "Giuseppe", "Verdi", "g.verdi@gmail.com", sha1("140898")));
-        $real2_utente = self::$account_dao->create(
-            new Utente(0, "Giuseppe", "Antoniani", "g.antoniani@gmail.com", sha1("140898"), true));
-        $real_genere = self::$genere_dao->create(
-            new Genere(0, "Fantasioso"));
-       // $real2_genere = self::$genere_dao->create(
-            //new Genere(0, "Comico"));
-        self::$userid = $real_utente->getID();
-        self::$user2id = $real2_utente->getID();
-        self::$genereid = $real_genere->getID();
-    }
+	public static function tearDownAfterClass(): void {
+		self::$account_dao->delete(self::$userid_normale);
+		self::$account_dao->delete(self::$userid_gestore);
+		if (self::$drammatico)
+			self::$genere_dao->delete(self::$drammatico->getID());
+		if (self::$inesistente)
+			self::$genere_dao->delete(self::$inesistente->getID());
+	}
 
-    public static function tearDownAfterClass(): void {
-        if (self::$userid !== null)
-            self::$account_dao->delete(self::$userid);
-        if (self::$user2id !== null)
-            self::$account2_dao->delete(self::$user2id);
-        if(self::$genereid !== null)
-            self::$genere_dao->delete(self::$genereid);
-    }
+	private function callController($user_id, $genere_id, $nome) {
+		$_COOKIE["userid"] = $user_id;
+		Auth::init();
+		$_POST["genere_id"] = $genere_id;
+		$_POST["nome"] = $nome;
+		ob_start();
+		include "../../controllers/gestione/Modifica genere.php";
+		$response = ob_get_contents();
+		ob_end_clean();
+		return $response;
+	}
 
-    private function callController($user_id, $nome, $genere) {
-        $_COOKIE["userid"] = $user_id;
-        Auth::init();
-        $_POST["nome"] = $nome;
-        $_POST["genere_id"] = $genere;
-        ob_start();
-        include "../../controllers/gestione/Modifica genere.php";
-        $response = ob_get_contents();
-        ob_end_clean();
-        return $response;
-    }
+	public function test_TC_5_4_1() {
+		$response = $this->callController(null, self::$inesistente->getID(), self::$inesistente->getNome());
+		$this->assertTrue(
+			Testing::assert_redirect($response, "/")
+		);
+	}
 
-    public function test_TC_5_4_1() {
-        $response = $this->callController(null, "Animazione", self::$genereid);
-        $this->assertTrue(
-            Testing::assert_redirect($response, "/")
-        );
-    }
+	public function test_TC_5_4_2() {
+		$response = $this->callController(self::$userid_normale, self::$inesistente->getID(), self::$inesistente->getNome());
+		$this->assertTrue(
+			Testing::assert_redirect($response, "/")
+		);
+	}
 
-    public function test_TC_5_4_2() {
-        $response = $this->callController(self::$userid, "Animazione", self::$genereid);
-        $this->assertTrue(
-            Testing::assert_redirect($response, "/")
-        );
-    }
+	public function test_TC_5_4_3() {
+		$response = $this->callController(self::$userid_gestore, self::$inesistente->getID(), "");
+		$this->assertTrue(
+			Testing::assert_block($response)
+		);
+	}
 
-    public function test_TC_5_4_3() {
-        $response = $this->callController(self::$user2id, "", self::$genereid);
-        $this->assertTrue(
-            Testing::assert_block($response)
-        );
-    }
+	public function test_TC_5_4_4() {
+		$response = $this->callController(self::$userid_gestore, self::$inesistente->getID(), "Drammatico");
+		$this->assertTrue(
+			Testing::assert_message($response, "Questo nome è associato ad un genere esistente")
+		);
+	}
 
-    public function test_TC_5_4_4() {
-        $response = $this->callController(self::$user2id, "Comico", self::$genereid);
-        $this->assertTrue(
-            Testing::assert_message($response, "Questo nome è associato ad un genere esistente")
-        );
-    }
-
-    public function test_TC_5_4_5() {
-        $response = $this->callController(self::$user2id, "Animalista", self::$genereid);
-        $this->assertTrue(
-            Testing::assert_redirect($response, "/genere.php?id=" . self::$genereid)
-        );
-
-    }
+	public function test_TC_5_4_5() {
+		$response = $this->callController(self::$userid_gestore, self::$inesistente->getID(), "Impensabile");
+		$this->assertTrue(
+			Testing::assert_redirect($response, "/genere.php?id=" . self::$inesistente->getID())
+		);
+	}
 
 }
