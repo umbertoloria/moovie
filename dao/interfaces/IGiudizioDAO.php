9d4f3a3a9b8c5c2daab9865628ad495b6ee830a9
Umberto Loria SAM <umberto.loria@gmail.com>
2020-01-18 19:06:17 +0100

    Introduzione specifica di <IGiudizioDAO> e <IPromemoriaDAO>

--- dao/interfaces/IGiudizioDAO.php
+++ dao/interfaces/IGiudizioDAO.php
@@ -2,20 +2,55 @@
 
 interface IGiudizioDAO {
 
+	/**
+	 * Aggiunge il giudizio fornito.
+	 * @param Giudizio $giudizio contiene le informazioni da salvare
+	 * @return bool Se non esiste un giudizio con UTENTE = $giudizio.UTENTE e FILM = $giudizio.FILM, le informazioni
+	 *                           contenute in $giudizio vengono salvate, e la funzione restituisce TRUE. Altrimenti
+	 *                           FALSE.
+	 */
 	public function create(Giudizio $giudizio): bool;
 
+	/**
+	 * Aggiorna il giudizio fornito.
+	 * @param Giudizio $giudizio contiene le informazioni da aggiornare
+	 * @return bool Se esiste un giudizio con UTENTE = $giudizio.UTENTE e FILM = $giudizio.FILM e VOTO !=
+	 *                           $giudizio.VOTO, allora le sue informazioni viene aggiornate e la funzione restitusice
+	 *                           TRUE. Altrimenti FALSE.
+	 */
 	public function update(Giudizio $giudizio): bool;
 
+	/**
+	 * Rimuove il giudizio fornito.
+	 * @param Giudizio $giudizio è il giudizio da rimuovere.
+	 * @return bool Se esiste un giudizio con UTENTE = $giudizio.UTENTE e FILM = $giudizio.FILM, allora viene
+	 *                           cancellato e la funzione restituisce TRUE. Altrimenti FALSE.
+	 */
 	public function delete(Giudizio $giudizio): bool;
 
 	/**
-	 * @param int[] $utenti_ids
-	 * @return Giudizio[]
+	 * Preleva tutti i giudizi espressi degli utenti forniti.
+	 * @param int[] $utenti_ids contiene gli ID degli utenti
+	 * @return Giudizio[] Restituisce un oggetto GIUDIZIO per ogni giudizio il cui UTENTE è presente in $utenti_ids.
 	 */
 	public function getAllOf(array $utenti_ids): array;
 
+	/**
+	 * Preleva (se esiste) il giudizio espresso dall'utente fornito verso il film fornito.
+	 * @param int $utente_id è l'ID dell'utente
+	 * @param int $film_id   è l'ID del film
+	 * @return Giudizio|null La funzione restituisce un oggetto GIUDIZIO contenente le informazioni del giudizio con
+	 *                       UTENTE = $utente_id e FILM = $film_id salvato. Se non esiste, restituisce NULL.
+	 */
 	public function get_from_utente_and_film(int $utente_id, int $film_id): ?Giudizio;
 
+	/**
+	 * Indica se esiste un giudizio espresso da un utente fornito verso un film fornito.
+	 * @param int $utente_id è l'ID dell'utente
+	 * @param int $film_id   è l'ID del film
+	 * @return bool Se esiste un giudizio con UTENTE = $utente_id e FILM = $film_id, la funzione restituisce TRUE.
+	 *                       Altrimenti FALSE.
+	 */
 	public function exists(int $utente_id, int $film_id): bool;
 
 }
