1d4c36e632dab938fff9c6d437a0b27c21613f46
Umberto Loria SAM <umberto.loria@gmail.com>
2020-01-18 19:15:09 +0100

    Introduzione specifica di <IRecitazioneDAO> e <IRegiaDAO>

--- dao/interfaces/IRecitazioneDAO.php
+++ dao/interfaces/IRecitazioneDAO.php
@@ -3,21 +3,34 @@
 interface IRecitazioneDAO {
 
 	/**
-	 * @param int $artista_id
-	 * @return Recitazione[]
+	 * Preleva tutte le recitazioni espresse da un artista fornito.
+	 * @param int $artista_id è l'ID dell'artista
+	 * @return Recitazione[] Restituisce un oggetto RECITAZIONE per ogni recitazione con ARTISTA = $artista_id.
 	 */
 	public function get_from_artista(int $artista_id): array;
 
 	/**
-	 * @param int $film_id
-	 * @return Recitazione[]
+	 * Preleva tutte le recitazioni nell'ambito di un film fornito.
+	 * @param int $film_id è l'ID del film
+	 * @return Recitazione[] Restituisce un oggetto RECITAZIONE per ogni recitazione con FILM = $film_id.
 	 */
 	public function get_from_film(int $film_id): array;
 
+
 	/**
-	 * @param int           $film_id
-	 * @param Recitazione[] $recitazioni
-	 * @return bool
+	 * Associa ad un film solo i generi forniti, disassociandolo con tutti gli altri.
+	 * @param int   $film_id           è l'ID del film
+	 * @param array $assign_genere_ids contiene gli ID dei generi da associare esclusivamente
+	 * @return bool Dopo l'esecuzione della funzione, il film con ID = $film_id sarà associato esclusivamente ai generi
+	 *                                 i cui ID sono presenti in $assign_genere_ids.
+	 */
+
+	/**
+	 * Associa ad un film solamente le recitazioni fornite.
+	 * @param int           $film_id     è l'ID del film
+	 * @param Recitazione[] $recitazioni contiene le recitazioni
+	 * @return bool Dopo l'esecuzione della funzione, le recitazioni del film con ID = $film_id saranno esclusivamente
+	 *              quelle fornite in $recitazioni.
 	 */
 	public function set_only(int $film_id, array $recitazioni): bool;
 
